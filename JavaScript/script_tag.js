851c3afad9a087fd34b6c1eaf2094c27278453be
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-09-15 21:50:07 +0530

    Add files via upload

---
+++ JavaScript/script_tag.js
@@ -0,0 +1,47 @@
+// JavaScript in <head> //
+
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+</head>
+<body>
+
+<h2>Demo JavaScript in Head</h2>
+
+<p id="demo">A Paragraph</p>
+<button type="button" onclick="myFunction()">Try it</button>
+
+</body>
+</html>
+
+
+// JavaScript in <body> //
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Demo JavaScript in Body</h2>
+
+<p id="demo">A Paragraph</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+
+</body>
+</html> 
+
+
+// External JavaScript //
+
+ <script src="myScript.js"></script> 
