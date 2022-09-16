4d5e1258d21aca066b9d9e52a775707e65c2fb7a
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-09-16 22:40:43 +0530

    Add files via upload

---
+++ JavaScript/variables.js
@@ -0,0 +1,22 @@
+Using var
+Using let
+Using const
+Using nothing
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+The two variables price1 and price2 are declared with the const keyword.
+These are constant values and cannot be changed.
