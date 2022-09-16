38719d13cb642a64c5b01160e7f22d9822316270
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-09-16 22:43:07 +0530

    Add files via upload

---
+++ JavaScript/objects.js
@@ -0,0 +1,12 @@
+Objects are variables too. But objects can contain many values.
+
+ const car = {type:"Fiat", model:"500", color:"white"}; 
+
+
+
+ const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
