b909f994139d6a61e18330298329dcd016d8c0e3
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-09-16 22:43:23 +0530

    Add files via upload

---
+++ JavaScript/functions.js
@@ -0,0 +1,17 @@
+A JavaScript function is a block of code designed to perform a particular task.
+
+A JavaScript function is executed when "something" invokes it (calls it).
+
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
+
+
+
+let x = myFunction(4, 3);   // Function is called, return value will end up in x
+
+function myFunction(a, b) {
+  return a * b;             // Function returns the product of a and b
+}
+
+
