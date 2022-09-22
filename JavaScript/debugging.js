d85f1ff1fe251f4d63394b8b0956032cddf109ac
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-09-22 22:49:38 +0530

    Add files via upload

---
+++ JavaScript/debugging.js
@@ -0,0 +1,28 @@
+/*Code Debugging
+
+Programming code might contain syntax errors, or logical errors.
+
+Many of these errors are difficult to diagnose.
+
+Often, when programming code contains errors, nothing will happen. There are no error messages, and you will get no indications where to search for errors.
+
+Searching for (and fixing) errors in programming code is called code debugging.*/
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First Web Page</h2>
+
+<p>Activate debugging in your browser (Chrome, IE, Firefox) with F12, and select "Console" in the debugger menu.</p>
+
+<script>
+a = 5;
+b = 6;
+c = a + b;
+console.log(c);
+</script>
+
+</body>
+</html> 
