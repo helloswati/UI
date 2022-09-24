253ff2faf785ac11a4875f93133c300f4420830e
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-09-24 21:31:09 +0530

    Add files via upload

---
+++ JavaScript/symbol_iterator.js
@@ -0,0 +1,39 @@
+/*The Symbol.iterator is a function that returns a next() function.
+
+An iterable can be iterated over with the code: for (const x of iterable) { } */
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+<h2>JavaScript Iterables</h2>
+
+<p id="demo"></p>
+
+<script>
+// Create an Object
+myNumbers = {};
+
+// Make it Iterable
+myNumbers[Symbol.iterator] = function() {
+  let n = 0;
+  done = false;
+  return {
+    next() {
+      n += 10;
+      if (n == 100) {done = true}
+      return {value:n, done:done};
+    }
+  };
+}
+
+let text = ""
+for (const num of myNumbers) {
+  text += num +"<br>"
+}
+
+document.getElementById("demo").innerHTML = text;
+</script>
+</body>
+</html>
