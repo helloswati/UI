b7a02ae19c90d57c21ea433a31387e569bdea547
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-11-10 23:50:53 +0530

    Add files via upload

---
+++ JavaScript/math.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Math</h2>
+
+<p>Math.PI returns PI:</p>
+<p id="piDemo"></p>
+
+<p>Math.sqrt(16) returns the square root of 16:</p>
+<p id="sqrtDemo"></p>
+
+<script>
+document.getElementById("piDemo").innerHTML = Math.PI;
+document.getElementById("sqrtDemo").innerHTML = Math.sqrt(16);
+</script>
+
+</body>
+</html>
