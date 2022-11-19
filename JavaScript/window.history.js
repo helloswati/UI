bff188df2d0b7d85530886b8f9377b48b0c1215b
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-11-19 23:36:20 +0530

    Add files via upload

---
+++ JavaScript/window.history.js
@@ -0,0 +1,40 @@
+The window.history object contains the browsers history.
+history.back() - same as clicking back in the browser
+history.forward() - same as clicking forward in the browser
+
+Window History Back
+
+<html>
+<head>
+<script>
+function goBack() {
+  window.history.back()
+}
+</script>
+</head>
+<body>
+
+<input type="button" value="Back" onclick="goBack()">
+
+</body>
+</html>
+
+
+
+Window History Forward
+
+
+<html>
+<head>
+<script>
+function goForward() {
+  window.history.forward()
+}
+</script>
+</head>
+<body>
+
+<input type="button" value="Forward" onclick="goForward()">
+
+</body>
+</html>
