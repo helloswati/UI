b5d94ee736f4911c572ec9900dcc71c9794783b4
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-12-01 22:49:18 +0530

    Add files via upload

---
+++ google_translator.ts
@@ -0,0 +1,34 @@
+
+const translations = {
+  en: {
+    header: "Book Your Ticket",
+    text1: "Complete your booking by following 2 steps",
+    text2: "Payment",
+    text3: "OTP Verification",
+    text4: "Verify OTP and Complete Your Booking"
+    text5: "Please verify the 6 digit OTP CODE send to send to Mobile Number and Email ID registered with your IRCTC user ID"
+    text6: "OTP has been sent to"
+    text7: "and"
+    text8: "Enter OTP"
+    text9: "Book Ticket"
+    text10: "Didn't receive the OTP"
+    text11: "Resend OTP"
+  },
+  hi: {
+    header: "अपना टिकट बुक करें",
+    text1: "2 चरणों का पालन करके अपनी बुकिंग पूरी करें",
+    text2: "भुगतान",
+    text3: "OTP सत्यापन",
+    text4: "OTP सत्यापित करें और अपनी बुकिंग पूरी करें"
+    text5: "कृपया अपने IRCTC उपयोगकर्ता आईडी के साथ पंजीकृत मोबाइल नंबर और ईमेल आईडी पर भेजने के लिए भेजे गए 6 अंकों के OTP कोड को सत्यापित करें"
+    text6: "OTP को भेज दिया गया है"
+    text7: "अन्य"
+    text8: "OTP दर्ज करें"
+    text9: "टिकट बुक करें"
+    text10: "OTP प्राप्त नहीं हुआ"
+    text11: "OTP पुनः भेजें"
+  },
+};
+
+
+
