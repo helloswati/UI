1732cb870cf433788f62fe02cf5bd87a6801bc77
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-12-13 23:55:19 +0530

    Add files via upload

---
+++ React/useEffect.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  });
+
+  return <h1>I have rendered {count} times!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Timer />);
